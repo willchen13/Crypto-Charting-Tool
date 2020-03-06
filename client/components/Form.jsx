2ac98c96cab681460a0330b8d2d27379046e4ba9
William Chen <williamchen13225@gmail.com>
2020-03-06 05:48:49 -0800

    Add input checking for form

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -7,7 +7,35 @@ const Form = ({changeDates}) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    changeDates(start,end);
+    if(start === '') {
+      alert('Please add a start time');
+    } else if(end === '') {
+      alert('Please add a end time');
+    } else if(start.length !== 10 || formatFailed(start)){
+      alert('Start date must be like: YYYY-MM-DD');
+    } else if(end.length !== 10 && formatFailed(end)){
+      alert('End date must be like: YYYY-MM-DD');
+    } else {
+      changeDates(start,end);
+    };
+  }
+
+  const formatFailed = (input) => {
+    const str = input.split('-');
+    if(str.length !== 3) {
+      return true;
+      //check if year is length 4 and a number greater than 2008 (year bitcoin was invented! one year after to be safe)
+    } else if(str[0].length !== 4 || isNaN(str[0]) || str[0] > 2008) {
+      return true;
+      //check if month is length 2 and a number between 1 and 12
+    } else if(str[1].length !== 2 || isNaN(str[1]) || (str[1] >= 1 && str[1] <= 12)) {
+      return true;
+      //check if day is a length 2 and a number between 1 and 31
+    } else if(str[2].length !== 2 || isNaN(str[2]) || (str[2] >= 1 && str[2] <= 31)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   const handleChange = (e) => {
@@ -17,6 +45,7 @@ const Form = ({changeDates}) => {
       setEnd(e.target.value);
     }
   }
+
 
   return(
     <>
